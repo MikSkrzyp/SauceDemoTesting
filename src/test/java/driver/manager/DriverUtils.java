7b8e50604e163461f2181db0c355d828718d71a4
Mikolaj Skrzypkowski <mikolajs620@gmail.com>
2024-02-06 17:25:11 +0100

    basic setup before work

---
+++ src/test/java/driver/manager/DriverUtils.java
@@ -0,0 +1,19 @@
+package driver.manager;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+public class DriverUtils {
+
+    @Step("Maximizing browser window")
+    public static void setInitialConfiguration(){
+        DriverManager.getWebDriver().manage().window().maximize();
+    }
+
+    @Step("Navigating to URL: {pageUrl}")
+    public static void navigateToPage(String pageUrl){
+        DriverManager.getWebDriver().navigate().to(pageUrl);
+    }
+
+}
